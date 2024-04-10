5ad870bec47bc294bd70b11782c294c6adb9841f
Daniel Rodrigues <danielrodriguescarvalho777@gmail.com>
2024-04-10 18:21:11 -0300

    using template and derivatives

--- src/App.vue
+++ src/App.vue
@@ -7,16 +7,7 @@ import CompositionApi from './components/CompositionApi.vue';
 </template>
 
 <style>
-* {
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
-  }
-  
-  body {
-    background-color: #1c1a1d;
-    color: #e5e5e5;
-  }
+
   
   #app {
     font-family: 'Ubuntu', sans-serif;
--- src/components/CompositionApi.vue
+++ src/components/CompositionApi.vue
@@ -2,11 +2,12 @@
 import { reactive } from 'vue';
 
 const state = reactive({
-    name: 'Lester',
-    login: 'skibidi',
-    bio: '...',
-    company:'Toilet Inc.',
-    avatar_url:'https://unsplash.it/256',
+    name: '',
+    login: '',
+    bio: '',
+    company:' ',
+    avatar_url:'',
+    repos:[],
     searchInput: ''
 })
 
@@ -19,16 +20,37 @@ async function fetchGithubUser(){
     state.bio = bio
     state.company = company
     state.avatar_url = avatar_url
+
+fetchUserRepositories(login)
+}
+
+async function fetchUserRepositories(username){
+    const res = await fetch(`https://api.github.com/users/${username}/repos`)
+    const repos = await res.json()
+
+    state.repos = repos
+    
 }
 </script>
 
 <template>
     <h2>GitHub User Data</h2>
     <input type="text" v-model="state.searchInput">
-    <button v-on:click="fetchGithubUser">Carregar Usuário</button>
-    <img v-bind:src="state.avatar_url">
-    <strong>@{{ state.login }}</strong>
-    <h1>{{ state.name }}</h1>
-    <h2>{{ state.company }}</h2>
-    <span>{{ state.bio }}</span>
+    <button @click="fetchGithubUser">Carregar Usuário</button>
+    <div v-if="state.login != ''">
+        <img :src="state.avatar_url">
+        <strong>@{{ state.login }}</strong>
+        <h1>{{ state.name }}</h1>
+        <h2>{{ state.company }}</h2>
+        <span>{{ state.bio }}</span>
+    </div>
+
+    <section v-if="state.repos.length > 0">
+      <article v-for="repo of state.repos">
+        <h3>{{ repo.full_name }}</h3>
+        <p>{{ repo.description }}</p>
+        <a :href="repo.html_url" target="_blank">Ver no GitHub</a>
+      </article>
+    </section>
+    
 </template>
